95159b406e9ef3970d3ce2fc7ea387e1a383e615
Uros Simic <simic.uros@gmail.com>
2017-07-25 12:26:34 +0200

    Setting photo from input stream without placeholder

--- ByakuGallery-lib/src/com/appculture/PanoramaImageView.java
+++ ByakuGallery-lib/src/com/appculture/PanoramaImageView.java
@@ -143,12 +143,27 @@ public class PanoramaImageView extends TouchImageView implements SensorEventList
 
     public void setPanoramaFromInputStream(InputStream is, @DrawableRes int placeholderRes, @Nullable TileBitmapDrawable.OnInitializeListener initializeListener) {
         setScaleTypeMatrixIfNeeded();
-        final Drawable placeHolder = ContextCompat.getDrawable(getContext(), placeholderRes);
+
+        Drawable placeHolder = null;
+        if (placeholderRes != 0) {
+            placeHolder = ContextCompat.getDrawable(getContext(), placeholderRes);
+        }
+
         TileBitmapDrawable.attachTileBitmapDrawable(this, is, placeHolder, initializeListener);
     }
 
     public void setPanoramaFromInputStream(InputStream is, @DrawableRes int placeholderRes) {
         setPanoramaFromInputStream(is, placeholderRes, null);
+    }
+
+    public void setPanoramaFromInputStream(InputStream is, @Nullable TileBitmapDrawable.OnInitializeListener initializeListener) {
+        setScaleTypeMatrixIfNeeded();
+        setPanoramaFromInputStream(is, 0, initializeListener);
+    }
+
+    public void setPanoramaFromInputStream(InputStream is) {
+        setScaleTypeMatrixIfNeeded();
+        setPanoramaFromInputStream(is, 0, null);
     }
 
     public void setPanoramaFromRes(@RawRes int rawResImage, @DrawableRes int placeholderRes, @Nullable TileBitmapDrawable.OnInitializeListener initializeListener) {
